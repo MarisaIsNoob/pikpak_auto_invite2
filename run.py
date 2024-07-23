cb088a70a6e5b0e1019b063a82c94704216539d5
Stuarygh <qingkunsun220@gmail.com>
2024-07-24 00:44:25 +0800

    Update run.py

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ from rich import print_json
 
 
 
+
 DEBUG_MODE = False  # Debug模式，是否打印请求返回信息
 # PROXY = input('请输入代理，如不需要直接回车:')  # 代理，如果多次出现IP问题可尝试将自己所用的魔法设置为代理。例如：使用clash则设置为 'http://127.0.0.1:7890'
 PROXY = ''
